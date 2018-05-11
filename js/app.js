5c18828aae4752489f75cdc2dd8366d73e34fa91
mechaphysis <triptofante@gmail.com>
2018-05-11 16:10:16 +0200

    test: Add test code for card event listener and draft for toggling card classes

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+for (card in cards) {
+  card.addEventListener('click', function test () {
+    console.log('This is a test');
+  })
+}
+/*
+cards[0].classList.toggle('show');
+cards[0].classList.toogle('open');
+*/
