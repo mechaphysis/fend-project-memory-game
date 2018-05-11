d3ef7371f01a76141c39d86f889022678e5877d1
mechaphysis <triptofante@gmail.com>
2018-05-11 17:37:10 +0200

    feat: Add for...of openCardsList, check if cards match, calls lockCards

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ function shuffle(array) {
  * set up the event listener for a card. If a card is clicked:
  *  x display the card's symbol (put this functionality in another function that you call from this one)
  *  x add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
+ *  x if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
@@ -49,6 +49,10 @@ function shuffle(array) {
   toggleClass(evt);
   openCards(evt);
   if (openCardsList.length != 0) {
-    
-  }
+    for (e of openCardsList) {
+      if (e.children == evt.target.children) {
+        lockCards(e,evt.target);
+      };
+    };
+  };
  });
