9fab99058b744a4fccfcc8ee6a24c0427a807835
mechaphysis <triptofante@gmail.com>
2018-06-19 17:44:19 +0200

    docs: Add more comments for detailing the JS functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //Create a timer for the game:
-
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
@@ -7,7 +6,6 @@ let seconds = 0;
 /*Simple padding function for displaying numbers in time format (two digit)
 From: https://stackoverflow.com/questions/6312993/
 */
-
 function leftPad(number, targetLength) {
     var output = number + '';
     while (output.length < targetLength) {
@@ -16,8 +14,10 @@ function leftPad(number, targetLength) {
     return output;
 }
 
+// Define a variable to store the HTML where the timer is for further modifying it with JS
 let timer = document.querySelector(".timer");
 
+// This function gathers the timer running, it adds with each iteration in seconds and hold two conditionals to add up minutes and hours
 function timerFunction() {
   timer.innerText =leftPad(hours,2)+":"+leftPad(minutes,2)+":"+leftPad(seconds,2);
   seconds++
@@ -31,6 +31,7 @@ function timerFunction() {
   }
 }
 
+//Start the timer
 let intervalTimer = setInterval(timerFunction, 1000);
 
 /*
@@ -41,26 +42,23 @@ let deck = document.querySelector('.deck');
 let cards = document.getElementsByClassName('card');
 
 // Create an array from the list of cards, in order to being able to shuffle them
-
 let cardsArr = [...cards];
 
 // Define reset button
-
 let reset = document.querySelector('.restart');
 
-// Define stars
-
+// Define variables for stars and counter of movements
 let stars = document.querySelector('.stars');
 let counter = document.querySelector('.moves');
 let count = 0;
 
+//Define function for incrementing the counter of moves
 function incremCounter(){
   count += 1;
   counter.innerText = count;
 };
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
@@ -81,7 +79,6 @@ function shuffle(array) {
 */
 
 // Apply the shuffle function and append each shuffled card to deck
-
 let shuffledCards = shuffle(cardsArr);
 
 shuffledCards.forEach(function(element){
@@ -99,13 +96,20 @@ deck.appendChild(element);
  *    x if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+// This function flips the card clicked and reveals its symbol
 function toggleClass (evt) {
   evt.target.classList.toggle('show');
   evt.target.classList.toggle('open');
 }
-
+// Define a variable for storing the array of Open Cards
 let openCardsList = [];
 
+/* This function controls the behaviour of opened cards.
+ * It adds the open card to an array, and checks for match in case of two cards opened.
+ * If the cards match it triggers the function lockCards() - so they stay open and locked -
+ * If the cards do not match it triggers the function removeCards():
+ *  -> refer to the definition of removeCards() for explicit explanation
+ */
 function openCards (evt) {
   openCardsList.push(evt.target);
      if (openCardsList.length == 2) {
@@ -119,8 +123,11 @@ function openCards (evt) {
      };
 };
 
-// functionality for decreasing Star Rating
-
+/*  Functionality for decreasing Star Rating:
+ *  With switch statements we select different cases.
+ *  When move counter reaches 10, it trigger the first case and decreases 1 one star, by changing its class.
+ *  When move counter reaches 20, it triggers the second case and decreases another star, again by changing its class.
+ */
 function starRating () {
   switch (count) {
       case 10:
@@ -137,12 +144,19 @@ function starRating () {
     }
 }
 
+/* This function is triggered when the two cards in the open list match.
+ * It adds to them the class ".match" that makes them stay revealed and locked (you cannot flip them back again)
+ */
 function lockCards () {
   openCardsList[0].classList.add('match');
   openCardsList[1].classList.add('match');
   openCardsList = [];
 };
 
+/* This function restores the opened cards to its original state
+ * it flips them back by removing the open and show classes
+ * And, it restores the OpenCardsList array to being empty
+ */
 function removeCards () {
   setTimeout(function(){
   openCardsList[0].classList.remove('show','open');
@@ -150,6 +164,9 @@ function removeCards () {
   openCardsList = [];}, 500);
 };
 
+/* This function restarts the game when the user clicks the reset button
+ * The explicit functionality is detailed below with each line:
+ */
 function restartGame() {
   console.log('click en reset');
   //Hidding all cards again
@@ -166,13 +183,17 @@ function restartGame() {
   //Shuffling cards again
   shuffledCards = shuffle(cardsArr);
   shuffledCards.forEach(function(element){
-    deck.appendChild(element);
+      deck.appendChild(element);
   });
 }
 
+// Define variables for matched count so we can track how many matches the user has and also a variable for the modal popup
 let matchedCount = 0;
 let modalPopup = document.querySelector('.popup');
 
+/* This function makes the modal pop up appear when all cards are matched.
+ * It also modifies its HTML to show the user final stats
+ */
 function matched() {
   modalPopup.style.visibility = "visible";
   document.querySelector('.timeStat').innerHTML=timer.innerHTML;
@@ -180,11 +201,14 @@ function matched() {
   document.querySelector('.moveStat').innerHTML=counter.innerHTML + " Moves";
  };
 
+// event listener for user clicks:
 deck.addEventListener('click',function (evt) {
+// if the click is in a card (not in the deck arround cards) it triggers the following functions:
   if (evt.target != deck) {
     toggleClass(evt);
     openCards(evt);
     starRating();
+    // if all cards are matched it triggers the following:
     if (matchedCount == 16) {
       //This stops the timer:
       clearInterval(intervalTimer);
@@ -194,12 +218,15 @@ deck.addEventListener('click',function (evt) {
   }
 });
 
+// Event listener for restarting the game when the user clicks the reset button
 reset.addEventListener('click',function(){
   restartGame();
 });
 
+// Define variable for storing the Try Again button in the Modal Popup
 let tryAgain = document.querySelector('.tryAgain');
 
+// Event listener for the Try Again button. It removes the popup from visibility and restarts the game
 tryAgain.addEventListener('click',function(){
   modalPopup.style.display="none";
   restartGame();
